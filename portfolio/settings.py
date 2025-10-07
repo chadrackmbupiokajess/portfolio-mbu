70bc8ebc4a9bde7d952b8d971b68a645ae067984
Chadrack Mbu Jess <116906647+chadrackmbujess@users.noreply.github.com>
2025-10-07 20:31:39 +0100

    csp

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'csp.middleware.CSPMiddleware',
+    #'csp.middleware.CSPMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
